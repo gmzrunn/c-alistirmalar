a1c3f50029bb3329534125fdd2e71a9abe53fe57
gamze <gmznurak@gmail.com>
2025-06-25 13:46:33 +0300

    csharp alıştırmaları

---
+++ 8/Program.cs
@@ -0,0 +1,10 @@
+﻿// This program filters and prints order IDs that start with the letter 'B'.
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
