f3b1501eabbd75c769542ea2ebde28030b8dd05d
gamze <gmznurak@gmail.com>
2025-06-25 14:13:34 +0300

    microsoftlearn alıştırmalar

---
+++ 9/Program.cs
@@ -0,0 +1,17 @@
+﻿// This program generates 5 random order IDs in the format of a letter followed by a three-digit number.
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
