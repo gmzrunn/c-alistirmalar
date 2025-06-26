7a220db3480741d1bcb5c767fa149f2aa36abdd3
gamze <gmznurak@gmail.com>
2025-06-26 15:28:23 +0300

    microsoftlearn alıştırmalar.

---
+++ 15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Description: FizzBuzz program that prints numbers from 1 to 100,
+// replacing multiples of 3 with "Fizz", multiples of 5 with "Buzz", 
+for (int i = 1; i < 101; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else
+        Console.WriteLine($"{i}");
+}
---
+++ 16/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+// This program generates random numbers between 1 and 10 until it generates the number 7.
+Random random = new Random();
+int current = 0;
+
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
+*/
+
+Random random = new Random();
+int current = random.Next(1, 11);
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
