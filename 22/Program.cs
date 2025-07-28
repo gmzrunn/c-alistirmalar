1027f3ab73a956e629d3a209317a8e7f72f1a502
gamze <gmznurak@gmail.com>
2025-07-28 22:18:47 +0300

    tüm alıştırmalar tamamlandı

---
+++ 22/Program.cs
@@ -0,0 +1,16 @@
+﻿// This program reverses the words in a given pangram.
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+string[] message = pangram.Split(' ');
+
+string[] newMessage = new string[message.Length];
+
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+}
+
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
---
+++ 24/Program.cs
@@ -0,0 +1,36 @@
+﻿// This program extracts a quantity from an HTML-like string, replaces a trade symbol with a registered symbol, and formats the output.
+using System;
+class Program
+{
+    static void Main()
+    {
+        const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+        string quantity = "";
+        string output = "";
+
+        const string openSpan = "<span>";
+        const string closeSpan = "</span>";
+
+        int quantityStart = input.IndexOf(openSpan) + openSpan.Length;
+        int quantityEnd = input.IndexOf(closeSpan);
+        int quantityLength = quantityEnd - quantityStart;
+        quantity = input.Substring(quantityStart, quantityLength);
+        quantity = $"Quantity: {quantity}";
+
+        const string tradeSymbol = "&trade;";
+        const string regSymbol = "&reg;";
+        output = input.Replace(tradeSymbol, regSymbol);
+
+        const string openDiv = "<div>";
+        int divStart = output.IndexOf(openDiv);
+        output = output.Remove(divStart, openDiv.Length);
+
+        const string closeDiv = "</div>";
+        int divCloseStart = output.IndexOf(closeDiv);
+        output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
+
+        Console.WriteLine(quantity);
+        Console.WriteLine(output);
+    }
+}
